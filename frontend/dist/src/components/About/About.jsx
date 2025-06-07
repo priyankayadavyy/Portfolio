538f44e85ebaf76e04fac8d1b45d0aa15b41ae12
Priyanka Singh Yadav <141716922+priyankayadavyy@users.noreply.github.com>
2025-06-07 12:40:25 +0530

    Update About.jsx

--- frontend/dist/src/components/About/About.jsx
+++ frontend/dist/src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./About.css";
 import photo from "../../assets/priyanka.jpg";
-import resume from "../../assets/Resume.pdf";
+import resume from "../../assets/priyankapsy.pdf";
 import { FiCheck } from "react-icons/fi";
 
 const About = () => {
